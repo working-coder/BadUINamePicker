05bdaf6c68ecba59dd8b7314b2fc793ba43c1991
working-coder <74379977+working-coder@users.noreply.github.com>
2021-10-01 15:11:44 +0300

    added the final build

---
+++ BadUIbattle/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace BadUIbattle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.txtName.Location = new System.Drawing.Point(274, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(713, 60);
+            this.txtName.TabIndex = 0;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(203, 1);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(216, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(-3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 351);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 350);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Name Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BadUIbattle/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadUIbattle
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+        private TrackBar[] _trackBars = new TrackBar[0];
+        private Label[] _labels = new Label[0];
+        int y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Controls.Add(trackBar1);
+            panel1.Controls.Add(txtName);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (_labels.Length != 0)
+            {
+                y = 0;
+                for (int i = 0; i <_trackBars.Length; i++)
+                {
+                    panel1.Controls.Remove(_trackBars[i]);
+                   
+                    panel1.Controls.Remove(_labels[i]);
+                  
+                }
+                _trackBars = new TrackBar[0];
+                _labels = new Label[0];
+            }
+            CreateLabeledTrackBars(this, trackBar1.Value);
+
+         
+        }
+ 
+        void CreateLabeledTrackBars(Control host, int n)
+        {
+            const int trackBarWidth = 150;
+            const int minValue = 0;
+            const int maxValue = 25;
+            const int defaultValue = 0;
+
+            _trackBars = new TrackBar[n];
+            _labels = new Label[n];
+            
+            for (int i = 0; i < n; ++i)
+            {
+                var label = new Label()
+                {
+                    Top = y,
+                    Left = trackBarWidth,
+                    Text = " ",
+                    Parent = host,
+                    Visible = false,
+                };
+                var trackBar = new TrackBar()
+                {
+                    Top = y,
+                    Width = trackBarWidth,
+                    // save associated label
+                    Tag = label,
+                    Minimum = minValue,
+                    Maximum = maxValue,
+                    Value = defaultValue,
+                    Parent = host,
+                };
+                // handle ValueChangedEvent
+                trackBar.ValueChanged += OnTrackBarValueChanged;
+                // apply vertical offset for next trackbar
+                panel1.Controls.Add(trackBar);
+                panel1.Controls.Add(label);
+                y += trackBar.Height;
+                _trackBars[i] = trackBar;
+                _labels[i] = label;
+            }
+        }
+
+        void OnTrackBarValueChanged(object sender, EventArgs e)
+        {
+            // get trackbar, which generated event
+            var trackBar = (TrackBar)sender;
+            // get associated label
+            var associatedLabel = (Label)trackBar.Tag;
+            if (trackBar.Value == 1)
+            {
+                associatedLabel.Text = "a";
+            }
+            else if (trackBar.Value == 2)
+            {
+                associatedLabel.Text = "b";
+            }
+            else if (trackBar.Value == 3)
+            {
+                associatedLabel.Text = "c";
+            }
+            else if (trackBar.Value == 4)
+            {
+                associatedLabel.Text = "d";
+            }
+            else if (trackBar.Value == 5)
+            {
+                associatedLabel.Text = "e";
+            }
+            else if (trackBar.Value == 6)
+            {
+                associatedLabel.Text = "f";
+            }
+            else if (trackBar.Value == 7)
+            {
+                associatedLabel.Text = "g";
+            }
+            else if (trackBar.Value == 8)
+            {
+                associatedLabel.Text = "h";
+            }
+            else if (trackBar.Value == 9)
+            {
+                associatedLabel.Text = "i";
+            }
+            else if (trackBar.Value == 10)
+            {
+                associatedLabel.Text = "k";
+            }
+            else if (trackBar.Value == 11)
+            {
+                associatedLabel.Text = "l";
+            }
+            else if (trackBar.Value == 12)
+            {
+                associatedLabel.Text = "m";
+            }
+            else if (trackBar.Value == 13)
+            {
+                associatedLabel.Text = "n";
+            }
+            else if (trackBar.Value == 14)
+            {
+                associatedLabel.Text = "o";
+            }
+            else if (trackBar.Value == 15)
+            {
+                associatedLabel.Text = "p";
+            }
+            else if (trackBar.Value == 16)
+            {
+                associatedLabel.Text = "r";
+            }
+            else if (trackBar.Value == 17)
+            {
+                associatedLabel.Text = "s";
+            }
+            else if (trackBar.Value == 18)
+            {
+                associatedLabel.Text = "t";
+            }
+            else if (trackBar.Value == 19)
+            {
+                associatedLabel.Text = "u";
+            }
+            else if (trackBar.Value == 20)
+            {
+                associatedLabel.Text = "v";
+            }
+            else if (trackBar.Value == 21)
+            {
+                associatedLabel.Text = "w";
+            }
+            else if (trackBar.Value == 22)
+            {
+                associatedLabel.Text = "x";
+            }
+            else if (trackBar.Value == 23)
+            {
+                associatedLabel.Text = "y";
+            }
+            else if(trackBar.Value == 24)
+            {
+                associatedLabel.Text = "z";
+            }
+            else if (trackBar.Value == 25)
+            {
+                associatedLabel.Text = " ";
+            }
+
+            
+
+            txtName.Text = "";
+            for (int i = 0; i < _labels.Length; i++)
+            {
+                txtName.Text += _labels[i].Text;
+            }
+            // recalculate sum of all values and update other label here
+        } 
+    
+    }
+}
